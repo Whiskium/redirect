cd6bf78e80f1e1a400cdaf68bd18ebd136477eb9
whiskium <zy.woo@outlook.com>
2024-04-05 11:56:54 +0800

    优化 回车事件

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function redirect() {
   let value = document.querySelector('input').value
   if (value.includes('://')) {
+    document.querySelector('iframe').setAttribute('src', value)
+    value = value.match(/[\w\d\.]+(?=:\/\/)/)
     document.querySelector('.card-info').innerHTML = `正在跳转至 ${value}`
     document.querySelector('.card-option').innerHTML = `取消`
     back()
-    document.querySelector('iframe').setAttribute('src', value)
   }
   else {
     alert('无效链接')
@@ -15,7 +16,7 @@ function input() {
   document.querySelector('.no-target').style.display = 'none'
   document.querySelector('.redirection').style.display = 'flex'
 }
-  
+
 function back() {
   document.querySelector('.no-target').style.display = 'flex'
   document.querySelector('.redirection').style.display = 'none'
@@ -29,4 +30,14 @@ function redirect_init() {
     document.querySelector('.card-info').innerHTML = `正在跳转至 ${queryString}`
     document.querySelector('.card-option').innerHTML = `取消`
   }
+}
+
+window.onload = function() {
+  redirect_init()
+  document.querySelector('input').addEventListener('keypress', function(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault()
+      redirect()
+    }
+  })
 }
