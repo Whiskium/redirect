0868c39d54985cf9a7ce25e900ea2e5e835d3276
whiskium <zy.woo@outlook.com>
2024-04-05 11:13:08 +0800

    优化 跳转逻辑 优化 提示逻辑 新增 网站图标

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function redirect() {
   let value = document.querySelector('input').value
   if (value.includes('://')) {
-    window.location.href = value
+    document.querySelector('.card-info').innerHTML = `正在跳转至 ${value}`
+    back()
+    document.querySelector('iframe').setAttribute('src', value)
   }
   else {
-    confirm('无效链接')
+    alert('无效链接')
   }
 }
 
@@ -13,7 +15,7 @@ function input() {
   document.querySelector('.redirection').style.display = 'flex'
 }
   
-  function back() {
+function back() {
   document.querySelector('.no-target').style.display = 'flex'
   document.querySelector('.redirection').style.display = 'none'
 }
@@ -21,9 +23,9 @@ function input() {
 function redirect_init() {
   let queryString = window.location.search.substring(1);
   if (queryString.includes('://')) {
-    window.location.href = queryString
+    document.querySelector('iframe').setAttribute('src', queryString)
     queryString = queryString.match(/[\w\d\.]+(?=:\/\/)/)
     document.querySelector('.card-info').innerHTML = `正在跳转至 ${queryString}`
-    document.querySelector('.card-option').innerHTML = `确定`
+    document.querySelector('.card-option').innerHTML = `取消`
   }
 }
